1ecf909783f60b83ae04938539afb46a1ec9ebc1
Michaelberry100 <walemichael100@gmail.com>
2024-05-15 00:24:17 +0100

    initial commit

---
+++ navigation/StackNavigation.js
@@ -0,0 +1,31 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import AppNavigation from "./AppNavigation";
+import LoginScreen from "../screens/LoginScreen";
+
+const Stack = createNativeStackNavigator();
+
+function Navigation() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Login" component={LoginScreen} options={{headerShown: false}} />
+                <Stack.Screen name="AppNavigation" component={AppNavigation} options={{ headerShown: false }} />
+                {/*<Stack.Screen name="Home" component={Home} />
+                <Stack.Screen name="Profile" component={Profile} />
+                <Stack.Screen name="Search" component={Search} />
+                <Stack.Screen name="Artist" component={Artist} />
+                <Stack.Screen name="Album" component={Album} />
+                <Stack.Screen name="Playlist" component={Playlist} />
+                <Stack.Screen name="Settings" component={Settings} />
+                <Stack.Screen name="Register" component={Register} />
+                <Stack.Screen name="CreatePlaylist" component={CreatePlaylist} />
+                <Stack.Screen name="CreateAlbum" component={CreateAlbum} />
+                <Stack.Screen name="CreateArtist" component={CreateArtist} />
+                <Stack.Screen name="CreatePlaylist" component={CreatePlaylist} />*/}
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+
+export default Navigation;
